36dcc9540a25c13156b3b53a12d17d8ee48366f5
savi <leosavis@gmail.com>
2022-09-15 23:43:18 -0300

    feat: Criação do projeto
    Projeto criado junto com uma activity HomeActivity.

---
+++ app/src/main/java/com/savi/portadecinema/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.savi.portadecinema.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.savi.portadecinema.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
